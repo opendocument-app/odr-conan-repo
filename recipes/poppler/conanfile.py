b11e1c928a8496840bd5cf2c995091ac42d26d1a
andiwand <stefl.andreas@gmail.com>
2022-07-07 23:40:03 +0200

    fix windows

--- recipes/poppler/conanfile.py
+++ recipes/poppler/conanfile.py
@@ -234,6 +234,8 @@ class PopplerConan(ConanFile):
     def _patch_sources(self):
         tools.patch(patch_file="patches/0001-fix-cmake-paths.patch",
                     base_path=self._source_subfolder)
+        tools.patch(patch_file="patches/0002-fix-windows.patch",
+                    base_path=self._source_subfolder)
         tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
             r"find_package(Cairo",
             r"find_package(cairo")
